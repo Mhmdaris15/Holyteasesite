a5a7a79f6b51ca673de842f205d212abd5f13d9f
Mhmdaris15 <muhammadaris1945@gmail.com>
2022-08-31 23:36:05 +0700

    Initialization Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('components.layouts.main')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Home Page</h1>
+            <p>{{ $content }}</p>
+            <small>You must be the change you wish to see in the world. - Mahatma Gandhi</small>
+        </div>
+    </div>
+</div>
+
+@endsection
